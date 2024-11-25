ef0d3671b9ce3bc0a15dea7911f2a33789c996fa
kimi <wlt879156580@gmail.com>
2024-11-26 03:50:28 +0800

    Edit colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -33,6 +33,11 @@ return {
     "Mofiqul/adwaita.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+      vim.g.adwaita_darker = false -- for darker version
+      vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+      vim.g.adwaita_transparent = true -- makes the background transparent
+    end,
   },
 
   {
