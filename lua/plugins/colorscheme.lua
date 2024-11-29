02f6e5b0427379fc0fcaa8a46dfab5a608421078
louloudu <70701369+louloudu@users.noreply.github.com>
2024-11-30 02:56:49 +0800

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,5 @@
 return {
+
   {
     "uloco/bluloco.nvim",
     lazy = false,
@@ -7,13 +8,24 @@ return {
     config = function()
       -- your optional config goes here, see below.
       require("bluloco").setup({
-        style = "light", -- "auto" | "dark" | "light"
-        transparent = false,
+        style = "dark", -- "auto" | "dark" | "light"
+        transparent = true,
         italics = true,
         terminal = vim.fn.has("gui_running") == 1, -- bluoco colors are enabled in gui terminals per default.
         guicursor = true,
       })
       vim.opt.termguicolors = true
+    end,
+  },
+
+  {
+    "Mofiqul/adwaita.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.adwaita_darker = false -- for darker version
+      vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+      vim.g.adwaita_transparent = true -- makes the background transparent
     end,
   },
 
@@ -27,17 +39,6 @@ return {
     "Yazeed1s/oh-lucy.nvim",
     lazy = false,
     priority = 1000,
-  },
-
-  {
-    "Mofiqul/adwaita.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.adwaita_darker = false -- for darker version
-      vim.g.adwaita_disable_cursorline = true -- to disable cursorline
-      vim.g.adwaita_transparent = true -- makes the background transparent
-    end,
   },
 
   {
