39427a227c4bb93685d617e6b82dcafae4b6dea8
kimi <wlt879156580@gmail.com>
2024-11-29 18:23:53 +0800

    arrow keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -64,3 +64,20 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("i", "<Down>", "<Down>", { buffer = true })
   end,
 })
+-- Enable arrow keys in the buffer for the neo-tree.nvim plugin
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "neo-tree",
+  callback = function()
+    vim.keymap.set("n", "<Left>", "<Left>", { buffer = true })
+    vim.keymap.set("n", "<Right>", "<Right>", { buffer = true })
+    vim.keymap.set("n", "<Up>", "<Up>", { buffer = true })
+    vim.keymap.set("n", "<Down>", "<Down>", { buffer = true })
+  end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "neo-tree-popup",
+  callback = function()
+    vim.keymap.set({ "n", "i" }, "<Left>", "<Left>", { buffer = true })
+    vim.keymap.set({ "n", "i" }, "<Right>", "<Right>", { buffer = true })
+  end,
+})
