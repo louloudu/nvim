9cf61f348ea2ea9e1c01664a8601aaae6bf8b3b8
louloudu <70701369+louloudu@users.noreply.github.com>
2025-02-14 16:57:03 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.cmake" },
+    { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
